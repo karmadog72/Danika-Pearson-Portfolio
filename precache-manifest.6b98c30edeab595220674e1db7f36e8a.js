bcbc6d186a78d8468a718a867e1ce5246a66857f
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2022-02-23 18:53:27 -0700

    Updates

---
+++ precache-manifest.6b98c30edeab595220674e1db7f36e8a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0886b328d1482070a2cb045fe7bb29",
+    "url": "/danika-pearson-portfolio/index.html"
+  },
+  {
+    "revision": "81bce29089440a630ae1",
+    "url": "/danika-pearson-portfolio/static/css/main.65ffce48.chunk.css"
+  },
+  {
+    "revision": "241ccd826cf5427f661b",
+    "url": "/danika-pearson-portfolio/static/js/2.6b4f2226.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/danika-pearson-portfolio/static/js/2.6b4f2226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9780b1659fed843608ab",
+    "url": "/danika-pearson-portfolio/static/js/3.68b6ee2a.chunk.js"
+  },
+  {
+    "revision": "81bce29089440a630ae1",
+    "url": "/danika-pearson-portfolio/static/js/main.a6d1474a.chunk.js"
+  },
+  {
+    "revision": "6a83501efe7cad63421f",
+    "url": "/danika-pearson-portfolio/static/js/runtime-main.cdfadd86.js"
+  },
+  {
+    "revision": "457603ab261f4cfd60591a2e2b878570",
+    "url": "/danika-pearson-portfolio/static/media/al-together.457603ab.jpg"
+  },
+  {
+    "revision": "9ba95b46f3eb96852359ff78d3fbdf0f",
+    "url": "/danika-pearson-portfolio/static/media/allergy-finder.9ba95b46.jpg"
+  },
+  {
+    "revision": "6de4dc8c01f3960640824d737d53a78f",
+    "url": "/danika-pearson-portfolio/static/media/cms-blog.6de4dc8c.jpg"
+  },
+  {
+    "revision": "1291cadf977e1752057bdd42d624dc3f",
+    "url": "/danika-pearson-portfolio/static/media/cover-image.1291cadf.jpg"
+  },
+  {
+    "revision": "3838593db2c9a43a93b87c515b9245d0",
+    "url": "/danika-pearson-portfolio/static/media/portfolio-challenge.3838593d.jpg"
+  },
+  {
+    "revision": "d0439c3723a107839e9dba59f1b04593",
+    "url": "/danika-pearson-portfolio/static/media/pswd-gen.d0439c37.jpg"
+  },
+  {
+    "revision": "73dc3b51464cda250747f49f5284b3ec",
+    "url": "/danika-pearson-portfolio/static/media/syntax.73dc3b51.jpg"
+  }
+]);
